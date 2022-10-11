61b975a7844f2c1fdb69a931564ee49977a45f66
andrew54068 <andrew0424718012@yahoo.com.tw>
2022-10-12 02:03:57 +0800

    feat: support system can't use combine.

--- Sources/Cadence/Value.swift
+++ Sources/Cadence/Value.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 import BigInt
+#if canImport(Combine)
 import Combine
+#else
+protocol TopLevelEncoder {
+    
+    /// The type this encoder produces.
+    associatedtype Output
+    
+    /// Encodes an instance of the indicated type.
+    ///
+    /// - Parameter value: The instance to encode.
+    func encode<T>(_ value: T) throws -> Self.Output where T : Encodable
+}
+
+extension JSONEncoder: TopLevelEncoder {}
+#endif
 
 public enum Value: Equatable {
     case void
